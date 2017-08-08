7c9a68aea40a7a6486a1342faa03cc9487a73130
Sergey <sergeyv@gmail.com>
2017-08-08 00:03:21 -0700

    Jinja2 template to generate site instead of hardcoding

--- server.py
+++ server.py
@@ -1,54 +1,55 @@
+import os
+from jinja2 import Environment, FileSystemLoader
+
 import cherrypy
 
 class Server(object):
     def __init__(self, db, cmd):
         self.db = db
         self.cmd = cmd
-        self.image_size=50
-
-    def print_this_motherfucker(self):
-        records = self.db.get_all_record()
-
-        all_records = []
-        for record in range(len(records)):
-            all_records.append(self.addtags(records[record], record))
-
-        top = """<html> <head> <link href="static/style.css" type="text/css" rel="stylesheet" /> <link href="https://mee6.xyz/static/css/bootstrap.css" type="text/css" rel="stylesheet" /> </head> <body> <div class="divTable paleBlueRows"> <div class="container"> <div class="row">
-              """
-        bot = "\n</div></div></div>\n</body>\n</html>"
+        self.PATH = os.path.dirname(os.path.abspath(__file__))
+        self.TEMPLATE_ENVIRONMENT = Environment(
+            autoescape=False,
+            loader=FileSystemLoader(os.path.join(self.PATH, 'templates')),
+            trim_blocks=False)
     
-        return top+"\n".join(all_records)+"\n"+bot
+    
+    def render_template(self, template_filename, context):
+        return self.TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
+   
 
-    def addtags(self, item, rank):
-        print(item)
-        xp_lvl = self.cmd.player_level(item[5])
-        print("xp level list: ",xp_lvl)
-        print("curr xp left: ",xp_lvl[1])
-        xp_thresh_to_next_lvl = self.cmd.calc_level_xp(xp_lvl[0])
-        xp_pct = int(xp_thresh_to_next_lvl/xp_lvl[1])*100
-        print("exp pct: ",xp_pct)
+    def get_users(self):
+        return self.db.get_all_record('display, disc, avatar, xp, role, rep')
 
-        line = """
-        \t<div class="col-md-1 col-sm-1 col-xs-1">{rank}</div>
-        \t<div class="col-md-1 col-sm-1 hidden-xs" style="padding:0"><img src="{avatar}" width={sz} height={sz} class="img-circle"></div>
-        \t<div class="col-md-4 col-sm-4 col-xs-5">{name} <small>#{disc}</small></div>
-        \t<div class="col-md-4 col-sm-4 col-xs-4"><center><h5>{left} / {thresh} <strong>XP</strong> [{exp}]</h5></center><div class="progress progress-striped"><div class="progress-bar progress-bar-info" style="width: {xp_pct}%"></div></div></div>
-        \t<div class="col-md-2 col-sm-2 col-xs-2"><h3>Reputation {rep}</h3></div>
-        \t<div class="col-md-2 col-sm-2 col-xs-2"><h3>Level {level}</h3></div>
-        </div>
+    def convert_user(self, user):
+        # Calculate players level and percent of exp in curr elvel
+        xp_lvl = self.cmd.player_level(user[3])
+        next_lvl = self.cmd.calc_level_xp(xp_lvl[0])
+        if xp_lvl[1] == 0:
+            xp_pct = 0
+        else:
+            xp_pct = 100 * float(xp_lvl[1])/float(next_lvl)
+        return {
+              "role":user[4],
+              "level":xp_lvl[0],
+              "name":user[0],
+              "disc":user[1],
+              "xp":user[3],
+              "xp_in_lvl":xp_lvl[1],
+              "xp_pct":xp_pct,
+              "xp_next_lvl":next_lvl,
+              "avatar":user[2],
+              "rep":user[5]}
 
-               """.format(
-                       rank=rank,
-                       avatar=item[7],
-                       name=item[4],
-                       disc=item[5],
-                       num=item[3],
-                       exp=item[6],
-                       left=xp_lvl[1],
-                       thresh=xp_thresh_to_next_lvl,
-                       xp_pct=xp_pct,
-                       rep=item[3],
-                       level=self.cmd.player_level(item[5])[0],
-                       sz=self.image_size)
-        return line
-
+    
+    def create_index_html(self, results):
+        context = { 'results': results }
+        return self.render_template('leaderboard.html', context)
+    
+    
+    def jinja2_main(self):
+        records = self.get_users()
+        results = []
+        for user in records:
+           results.append(self.convert_user(user))
+        return self.create_index_html(results)
